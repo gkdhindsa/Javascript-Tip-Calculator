c964975dfd2dae2f3177050992a6ab0300e3c12c
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-07-01 22:11:22 +0530

    basic html and js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let val;
+function calculate(){
+    let a=amount.value;
+    let t=type.value;
+    let p=people.value;
+     
+    console.log(a);
+
+    val=(a*0.01*t)/p;
+    console.log(val);
+    document.querySelector("#tip").innerText=val;
+}
+
+let amount=document.querySelector("#amount");
+let type=document.querySelector("#type");
+let people=document.querySelector("#people");
+document.querySelector("#submit").addEventListener('click', calculate);
